9689f09160b6e3ab99e5992b2ada535a5ce4acf6
Lindsay Hodge <lindsayhodge@utexas.edu>
2022-06-12 21:18:50 -0500

    Challenge 7 Deliverable 2

--- Queries/employee_database_challenge.sql
+++ Queries/employee_database_challenge.sql
@@ -55,3 +55,30 @@ GROUP BY unique_titles.title
 ORDER BY COUNT (unique_titles.emp_no) DESC;
 
 SELECT * FROM retiring_titles;
+
+--Deliverable 2
+CREATE TABLE dept_emp (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_elligibility
+FROM employees
+	INNER JOIN dept_emp
+	ON (employees.emp_no = dept_emp.emp_no)
+	INNER JOIN titles 
+	ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (dept_emp.to_date= '9999-01-01')
+ORDER BY employees.emp_no;
+
+SELECT * FROM mentorship_elligibility;
